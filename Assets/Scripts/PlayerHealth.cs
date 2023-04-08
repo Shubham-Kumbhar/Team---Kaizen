02d0a0cfe170a4a74b002259128556248904c3a5
Shubham-Kumbhar <shubhamkumbhar1911@gmail.com>
2023-04-08 17:10:28 +0530

    DieAnimation

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -7,13 +7,17 @@ public class PlayerHealth : MonoBehaviour
     // Start is called before the first frame update
     public int noOfLives=3; 
     public GameObject[] liveHearts;
+    public GameObject destroyShipAnim;
     // Update is called once per frame
     void Update()
     {
         if (noOfLives == 0)
         {
-            Time.timeScale = 0;
-            SceneManager.LoadScene("GameOver");
+            this.gameObject.SetActive(false);
+            Instantiate(destroyShipAnim, this.transform.position, Quaternion.identity);
+
+            //Time.timeScale = 0;
+            //SceneManager.LoadScene("GameOver");
         } 
        
     }
